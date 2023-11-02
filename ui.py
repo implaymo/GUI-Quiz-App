d858d2814c34ee23fe9377acc778bfe564f146c4
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-02 11:37:56 +0000

    Added text

--- ui.py
+++ ui.py
@@ -12,10 +12,8 @@ class QuizInterFace:
         self.window.configure(bg=THEME_COLOR)
         self.canvas = Canvas(self.window, width=300, height=250, bg="white")
         self.canvas.grid(column=0, row=1, columnspan=2, padx=20, pady=20)
-
         # Add padding at the top of the window
         self.window.grid_rowconfigure(0, minsize=50)
-
         # Images
         self.true_image = PhotoImage(file="images/true.png")
         self.false_image = PhotoImage(file="images/false.png")
@@ -24,5 +22,12 @@ class QuizInterFace:
         self.right_button.grid(column=0, row=2)
         self.false_button = Button(self.window, image=self.false_image)
         self.false_button.grid(column=1, row=2)
+        # Score text
+        self.score_text = Label(self.window, text="Score: 0 ", font=("Arial", 15, "italic"), bg=THEME_COLOR, fg="white")
+        self.score_text.grid(column=1, row=0, sticky="S")
+
+        # Question text
+        self.canvas_text = self.canvas.create_text(150, 125, text="Question", font=("Arial", 20, "italic"))
+
 
         self.window.mainloop()
