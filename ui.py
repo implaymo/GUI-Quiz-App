9bca7fc623e676fbd32378dd4b3946e47e9ae495
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-02 10:47:59 +0000

    Finished canvas and buttons UI

--- ui.py
+++ ui.py
@@ -8,13 +8,21 @@ class QuizInterFace:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.geometry("400x600")
+        self.window.geometry("350x460")
         self.window.configure(bg=THEME_COLOR)
         self.canvas = Canvas(self.window, width=300, height=250, bg="white")
-        self.canvas.grid(column=0, row=0, columnspan=1, padx=20, pady=20)
+        self.canvas.grid(column=0, row=1, columnspan=2, padx=20, pady=20)
 
-        # Configure the grid to expand the canvas
-        self.window.grid_rowconfigure(0, weight=1)
-        self.window.grid_columnconfigure(0, weight=1)
+        # Add padding at the top of the window
+        self.window.grid_rowconfigure(0, minsize=50)
+
+        # Images
+        self.true_image = PhotoImage(file="images/true.png")
+        self.false_image = PhotoImage(file="images/false.png")
+        # Buttons
+        self.right_button = Button(self.window, image=self.true_image)
+        self.right_button.grid(column=0, row=2)
+        self.false_button = Button(self.window, image=self.false_image)
+        self.false_button.grid(column=1, row=2)
 
         self.window.mainloop()
