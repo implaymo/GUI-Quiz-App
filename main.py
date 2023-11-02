5fceb6c4fbca78ed547379dd69043a57370646d3
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-02 23:02:19 +0000

    Made text question show in different lines

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_text)
-
 
 quiz = QuizBrain(question_bank)
 quiz_ui = QuizInterFace(question_text)
