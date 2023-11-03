e3aef6ae8e71c37566c3fdb3850f1cc15a46aa6f
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-03 15:41:44 +0000

    Added four functions at the Ui file, to check if the decision of the user, update score and to change canvas background for one second if the question is right or wrong

--- ui.py
+++ ui.py
@@ -9,6 +9,7 @@ class QuizInterFace:
 
     def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
+
         # User answer
         self.user_answer = None
 
@@ -29,15 +30,16 @@ class QuizInterFace:
 
         # Buttons
         self.true_button = Button(self.window, image=self.true_image,
-                                  highlightthickness=0, command=self.true_button_answer)
+                                  highlightthickness=0, command=self.true_button_pressed)
         self.true_button.grid(column=0, row=2)
 
         self.false_button = Button(self.window, image=self.false_image,
-                                   highlightthickness=0, command=self.false_button_answer)
+                                   highlightthickness=0, command=self.false_button_pressed)
         self.false_button.grid(column=1, row=2)
 
         # Score text
-        self.score_text = Label(self.window, text=f"Score: {self.quiz.score}", font=("Arial", 15, "italic"), bg=THEME_COLOR, fg="white")
+        self.score_text = Label(self.window, text=f"Score: {self.quiz.score}",
+                                font=("Arial", 15, "italic"), bg=THEME_COLOR, fg="white")
         self.score_text.grid(column=1, row=0, sticky="S")
 
         # Question text
@@ -49,20 +51,33 @@ class QuizInterFace:
         self.window.mainloop()
 
     def get_next_question(self):
+        """Gets next question"""
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
 
-    def true_button_answer(self):
+    def true_button_pressed(self):
+        """Checks if true button got pressed"""
         self.user_answer = "True"
         self.quiz.check_answer(user_answer=self.user_answer)
         self.update_score()
         self.get_next_question()
 
-    def false_button_answer(self):
+    def false_button_pressed(self):
+        """Checks if false button got pressed"""
         self.user_answer = "False"
         self.quiz.check_answer(user_answer=self.user_answer)
         self.update_score()
         self.get_next_question()
 
     def update_score(self):
+        """Update score"""
         self.score_text.config(text="Score: " + str(self.quiz.score))
+        if self.quiz.current_question.answer == self.user_answer:
+            self.canvas.config(bg="green")
+            self.window.after(1000, self.revert_background_color)
+        else:
+            self.canvas.config(bg="red")
+            self.window.after(1000, self.revert_background_color)
+
+    def revert_background_color(self):
+        self.canvas.config(bg="white")
